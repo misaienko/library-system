57a97816a4795080e5823a2c663cd7618a9d094e
Maryna Isaienko <macprobp@gmail.com>
2024-05-12 13:20:33 -0700

    Initial Commit

---
+++ LibrarySystem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LibrarySystem
+//
+//  Created by Admin on 12/05/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
